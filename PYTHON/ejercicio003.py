bd5f815571b09376ed46cf78ce59d1c92811c283
WilmerAlava2001 <wilmer.alavad@ug.edu.ec>
2021-06-20 09:57:17 -0500

    Ejercicio modificado

--- PYTHON/ejercicio003.py
+++ PYTHON/ejercicio003.py
@@ -1,1 +1,5 @@
-#github
+edad = int(input("Ingrese su edad: \t"))
+if(edad > 17):
+    print("Mayor de edad")
+else:
+    print("Menor de edad")
---
+++ PYTHON/ejercicio006.py
@@ -0,0 +1,8 @@
+#Ingrese un numero y diga si es par e impar
+"""dotstring
+1. pedir al usuario ingresar un numero
+2. hacer el calculo para ver si es par o impar (variable % 2 == 0 es par
+3. mostrar el mensaje
+"""
+numero = int(input("Ingrese el numero"))
+if numero mod 2
