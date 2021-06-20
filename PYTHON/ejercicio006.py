6655913679c02e352d3bca8b6566bc7b90eef981
WilmerAlava2001 <wilmer.alavad@ug.edu.ec>
2021-06-20 10:53:48 -0500

    Ejercicio 006

--- PYTHON/ejercicio006.py
+++ PYTHON/ejercicio006.py
@@ -4,5 +4,11 @@
 2. hacer el calculo para ver si es par o impar (variable % 2 == 0 es par
 3. mostrar el mensaje
 """
-numero = int(input("Ingrese el numero"))
-if numero mod 2
+numero = int(input("Ingrese el numero: \t"))
+
+if(numero == 0): # si el numero es igual a cero
+    print("Es cero")
+elif(numero % 2 == 0): # caso contrario si el numero mod 2 es cero
+    print("El numero es par")
+else: # caso contrario
+    print("El numero es impar")
